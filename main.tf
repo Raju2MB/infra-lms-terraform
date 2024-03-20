2b4846c9413fa886ab0a8e39aa3c2cf8fea94d5d
Raju2MB <rajumb43@gmail.com>
2024-03-20 20:10:24 +0530

    ec2 name updated

--- main.tf
+++ main.tf
@@ -213,6 +213,10 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   subnet_id = aws_subnet.IBM-web-subnet.id
   vpc_security_group_ids =[aws_security_group.IBM-web-sg.id]
+
+  tags = {
+    Name = "web-server"
+  }
 }
 
 
