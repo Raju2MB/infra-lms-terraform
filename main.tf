9495be3b9160e245998fbdeeba2e7c497f166363
Raju2MB <rajumb43@gmail.com>
2024-03-20 23:38:20 +0530

    0

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "IBM-web-rt" {
   vpc_id = aws_vpc.IBM-vpc.id
 
   route { 
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.IBM-Igw.id
   }
 
