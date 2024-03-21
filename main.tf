d103c5069eac6e231151cd0683fdaa09d8ae11d9
Raju2MB <rajumb43@gmail.com>
2024-03-21 11:34:22 +0530

    volume updated

--- main.tf
+++ main.tf
@@ -213,6 +213,7 @@ resource "aws_instance" "Ansible-master" {
   instance_type = "t2.medium"
   subnet_id = aws_subnet.IBM-web-subnet.id
   vpc_security_group_ids =[aws_security_group.IBM-web-sg.id]
+  size = 30
 
   tags = {
     Name = "Ansible-master"
