ca36903b08bc391a71664fe9a9f72bbdbc0e3733
Raju2MB <rajumb43@gmail.com>
2024-03-22 14:14:31 +0530

    upaste

--- install_sonarqube_nexus.sh
+++ install_sonarqube_nexus.sh
@@ -1,25 +1,10 @@
-#!/bin/bash
 #install docker
-# Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu  
+newgrp docker
 sudo chmod 777 /var/run/docker.sock
-
-#install sonarqube
-sudo apt update -y
-docker run -d --name sonarqube -p 9000:9000 sonarqube:latest
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 # install nexus
 sudo apt update -y
