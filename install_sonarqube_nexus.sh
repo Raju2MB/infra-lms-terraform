957fb7d6ace0ca106f4184b69d541228fe1d2f95
Raju2MB <rajumb43@gmail.com>
2024-03-23 08:32:05 +0530

    update

--- install_sonarqube_nexus.sh
+++ install_sonarqube_nexus.sh
@@ -5,9 +5,3 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu  
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
-docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
-
-# install nexus
-sudo apt update -y
-docker run -d -p 8081:8081 --name nexus sonatype/nexus3
-
