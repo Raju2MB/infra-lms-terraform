e4313f76e294d6c895cae36b89c974b27687cfcc
Raju2MB <rajumb43@gmail.com>
2024-03-23 08:39:12 +0530

    sonarqube container

--- install_sonarqube_nexus.sh
+++ install_sonarqube_nexus.sh
@@ -5,3 +5,6 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu  
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+
+# create sonarqube
+docker container run -dt --name sonarqube -p 9000:9000 sonarqube:latest
